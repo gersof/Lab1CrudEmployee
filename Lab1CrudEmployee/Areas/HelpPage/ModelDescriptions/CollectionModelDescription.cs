b64d5b1673fc6e70b338c9e27ad61ad716191d1f
Juan Esteban Angulo Uribe <jangulorama@gmail.com>
2018-12-17 20:38:57 -0500

    Initial project

---
+++ Lab1CrudEmployee/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Lab1CrudEmployee.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Lab1CrudEmployee/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Lab1CrudEmployee.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
